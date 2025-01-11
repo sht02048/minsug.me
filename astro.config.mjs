1632485b030cd79842d6c14b3a4cc832c358309b
sht02048 <sht02048@gmail.com>
2025-01-11 23:25:46 +0900

    📦️ Astro mdx 설치

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), mdx()],
 });
