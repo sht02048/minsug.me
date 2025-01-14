f13cb9aa797e7b41e9fb57b07a252af1a3a75c05
sht02048 <sht02048@gmail.com>
2025-01-14 21:27:01 +0900

    🔧 Remark 및 rehype 설정

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,14 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import remarkBreaks from "remark-breaks";
+import rehypePrettyCode from "rehype-pretty-code";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), mdx()],
+  markdown: {
+    remarkPlugins: [remarkBreaks],
+    rehypePlugins: [rehypePrettyCode],
+  },
 });
