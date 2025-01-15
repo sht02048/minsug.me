dab99693b821c3738c07348dbc78d15f1c5ad9a9
sht02048 <sht02048@gmail.com>
2025-01-15 20:07:34 +0900

    🔧 Tailwind Typography 설정

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,48 @@ export default {
         primary: "#eeeeec",
         secondary: "#b5b3ad",
       },
+      spacing: {
+        thin: "0.5px",
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            h1: {
+              marginLeft: 0,
+              marginBottom: 32,
+              fontWeight: 600,
+              fontSize: "1rem",
+            },
+            h2: {
+              margin: 0,
+              fontSize: "1rem",
+              fontWeight: 600,
+              scrollMarginTop: "5rem",
+            },
+            p: {
+              margin: 0,
+              opacity: 90,
+              lineHeight: 1.65,
+            },
+            "code::before": {
+              content: "none",
+            },
+            "code::after": {
+              content: "none",
+            },
+            "p > code": {
+              borderRadius: "0.375rem",
+              borderWidth: "1px",
+              borderColor: "#282826",
+              backgroundColor: "#181817",
+              padding: "2px 3.6px",
+              fontSize: "0.8571429em",
+              color: "#eeeeec",
+              fontWeight: 400,
+            },
+          },
+        },
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
